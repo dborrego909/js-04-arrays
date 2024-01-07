da3da29e796ae558d53a8984f3fe24ed9d8b2c7a
Daniel Borrego <154102665+dborrego909@users.noreply.github.com>
2024-01-06 20:20:48 -0800

    created a loop that listed fruits in an array in an unordered

--- main.js
+++ main.js
@@ -4,3 +4,13 @@ document.getElementById("demo").innerHTML = fruits;
 
 let size = fruits.length;
 document.getElementById("size").innerHTML = size;
+
+let fruitsLength = fruits.length;
+
+let list = "<ul>";
+for (let i = 0; i < fruitsLength; i++) {
+    list += "<li>" + fruits[i] + "</li>";
+}
+list += "</ul>";
+
+document.getElementById("list").innerHTML = list;
