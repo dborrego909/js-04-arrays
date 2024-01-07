446d0179d35a94c15335a59424547347b4fef818
Daniel Borrego <154102665+dborrego909@users.noreply.github.com>
2024-01-06 19:59:30 -0800

    Created html boiler and js. practiced arrays and sending it to html

---
+++ main.js
@@ -0,0 +1,6 @@
+const fruits = ["banana", "apple", "oranges", "kiwi"];
+
+document.getElementById("demo").innerHTML = fruits;
+
+let size = fruits.length;
+document.getElementById("size").innerHTML = size;
